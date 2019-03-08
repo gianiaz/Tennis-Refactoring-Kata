2316946614778dc2e90552f40306eaa770a35ee1
Giovanni Lenoci <giovanni.lenoci@facile.it>
2019-03-08 14:55:10 +0100

    Adds exctepion for deuce in advantage

--- php/Advantage.php
+++ php/Advantage.php
@@ -5,6 +5,7 @@ class Advantage extends AbstractStrategy
 {
     public function getDescription(Player $player1, Player $player2): string
     {
+
         $diff = abs($player1->getScore() - $player2->getScore());
 
         if($diff > 1) {
@@ -16,6 +17,10 @@ class Advantage extends AbstractStrategy
     }
 
     private function getWinner(Player $player1, Player $player2): Player {
+
+        if($player1->getScore() === $player2->getScore()) {
+            throw new InvalidArgumentException('Can\'t decide winner');
+        }
 
         if($player1->getScore() > $player2->getScore()) {
             return $player1;
